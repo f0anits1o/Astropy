f3b2cd4dc29959bd29626b72a8beac6bfc1b6ae1
f0anits1o <avotriniainafidelis@gmail.com>
2023-03-04 06:02:43 +0300

    m/s -> pc/yr

--- Getting started.py
+++ Getting started.py
@@ -9,3 +9,7 @@ print(const.c)
 print(       )
 print('Change Unity, m/s -> km/s')
 print('c = ', const.c.to('km/s'))
+
+print(    )
+print('Change Unity, m/s -> pc/yr')
+print('c = ', const.c.to('pc/yr'))
